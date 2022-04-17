aab24adb8c22eeff439a94f29c1a42e1bf709d21
ssalvi8586 <ssalvi8586@gmail.com>
2022-04-17 23:42:49 +0600

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import banner from "../../../images/banner.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <img src={banner} className="w-100" alt="banner"></img>
+    </div>
+  );
+};
+
+export default Banner;
